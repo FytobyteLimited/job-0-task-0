6b5aa29cc27964db890c450503b4156242a802aa
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-03 16:49:33 +0600

    service section completed

--- Aahmed-Hossain/challenge-1/src/components/service/Service.jsx
+++ Aahmed-Hossain/challenge-1/src/components/service/Service.jsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import React from "react";
+import { TbSettingsCog } from "react-icons/tb";
+import "./styles.css";
+import { BsCloudSun } from "react-icons/bs";
+import { LuCode2 } from "react-icons/lu";
+import Image from "next/image";
+import service1 from "../../../public/images/service-1.png";
+import service2 from "../../../public/images/service-2.png";
+import ButtonRounded from "@/shared/ButtonRounded";
+{
+  /* <LuCode2 /> */
+}
 const Service = () => {
   return (
-    <div>Service</div>
-  )
-}
+    <div className="  mt-4 w-11/12 lg:10/12 mx-auto ">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <ButtonRounded text={"Our Service"} Icon={TbSettingsCog} />
+        <h1 className="text-center font-extrabold text-5xl text-gradient mt-3">
+          Transform your tech <br />
+          business with tailored <br />
+          services
+        </h1>
+        <p className="text-normal lg:text-lg text-[#737373] text-center mb-6">
+          Websites / Applications / Web3 / Blockchain
+        </p>
+      </div>
+      {/* cards */}
+      <div className="flex flex-col lg:flex-row gap-8 justify-center  ">
+        {/* with cloud icon card */}
+        <div className="flex  flex-col  gap-2 p-4 rounded-xl border  bg-[#F5F5F5] w-7/12 lg:w-4/12 mx-auto ">
+          <h3>
+            <BsCloudSun />
+          </h3>
+          <h4 className="font-bold tet-xl dark:text-white">Lorem Ipsum</h4>
+          <p className="text-xs">Create Fast And Simple</p>
+          <p className="text-xs mt-2">
+            Lorem ipsum dolor sit amet... consectetur adipisicing elit. Sit, eos
+            voluptatem quo laborum perferendis eligendi voluptas nam dignissimos
+            hic dolorem inventore…
+          </p>
+        </div>
+        {/* long width card */}
+        <div className="w-full h-52 lg:h-block lg:w-8/12 mx-auto rounded-xl">
+          <Image
+            className="h-full w-full rounded-xl"
+            src={service1}
+            alt="service card"
+            height={500}
+            width={500}
+          />
+        </div>
+      </div>
+      {/* second line service cards */}
+      <div className="flex flex-col lg:flex-row gap-8 my-8">
+      
+          <div className="flex  lg:flex-row gap-4 lg:gap-8 w-full lg:w-8/12">
+            {/*2nd image card */}
+            <div className="w-1/2  h-48 lg:h-block mx-auto rounded-xl">
+              <Image
+                className="h-full w-full rounded-xl"
+                src={service2}
+                alt="service card"
+                height={500}
+                width={500}
+              />
+            </div>
+            {/* with code icon card */}
+            <div className="flex flex-col gap-2 p-2 lg:p-4 rounded-xl border  bg-[#F5F5F5] w-1/2  mx-auto ">
+              <h3>
+                <LuCode2 />
+              </h3>
+              <h4 className="font-bold tet-xl dark:text-white">
+                Custom Web Development
+              </h4>
+              <p className="text-xs">SPA / MPA</p>
+              <p className="text-xs mt-2">
+                Tailored website solutions to meet specific business needs and
+                goals.
+              </p>
+            </div>
+          </div>
 
-export default Service
+          {/* with code and bg gradiant card */}
+          <div className="flex flex-col gap-2 p-4 rounded-xl border  bg-[#F5F5F5] mx-auto bg-gradiant w-6/12 lg:w-4/12">
+            <h3>
+              <LuCode2 />
+            </h3>
+            <h4 className="font-bold tet-xl dark:text-white">
+              Custom Web Development{" "}
+            </h4>
+            <p className="text-xs">SPA / MPA</p>
+            <p className="text-xs mt-2">
+              Tailored website solutions to meet specific business needs and
+              goals.
+            </p>
+          </div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Service;
---
+++ Aahmed-Hossain/challenge-1/src/shared/ButtonRounded.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+const ButtonRounded = ({ text, Icon }) => {
+  return (
+    <button className="flex gap-3 rounded-full justify-center items-center py-2 w-[10rem] border border-black font-semibold text-md">
+      <p>{text}</p>
+      {Icon && <Icon />}
+    </button>
+  );
+};
+
+export default ButtonRounded;
